9367b1730a84447dd841b894c200510b16d72a3a
Singh-csm <csm8107@gmail.com>
2023-04-04 20:44:58 +0530

    airbnb-init

--- app/page.tsx
+++ app/page.tsx
@@ -2,90 +2,24 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      
+      {/* <p className='text-rose-600 text-2xl'>Hello Airbnb!</p>
+      <h1 className='text-blue-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-red-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-green-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-pink-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-yellow-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-cyan-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-black-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-brown-600 text-3xl'>HEllo Wonderful people!</h1>
+      <h1 className='text-orange-600 text-3xl'>HEllo Wonderful people!</h1> */}
+     
+    </div>
   )
 }
